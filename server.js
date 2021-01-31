94b0ce75f632eb5662fc336e9c1e40633cc30eb5
sugerhater <kehong0926@gmail.com>
2021-01-31 00:50:58 -0500

    set up server.js file

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require("express");
+const db = require("../original-homework1/osu-col-fsf-pt-10-2020-u-c/14-Full-Stack/01-Activities/07-ORM-To-Sequelize/Solved/models/index.js");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+require("./routes/api-routes.js")(app);
+
+db.sequelize.sync().then(function(){
+  app.listen(PORT, function(){
+    
+  })
+})
