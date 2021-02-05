c7eea90ab7ee7a10d19d2ed9077e494d9b40b840
sugerhater <kehong0926@gmail.com>
2021-02-04 23:21:45 -0500

    more update

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
 
 db.sequelize.sync().then(function(){
   app.listen(PORT, function(){
